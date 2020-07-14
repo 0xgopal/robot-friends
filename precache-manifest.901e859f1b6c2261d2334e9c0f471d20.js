1a982cd0ebc333e9235fb9dd3e68ad9d4ecaf396
durrdaddy <nvashistha237@gmail.com>
2020-07-14 12:56:45 +0530

    Updates

---
+++ precache-manifest.901e859f1b6c2261d2334e9c0f471d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799b38d20bf50e768d63fefd87e24031",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/robot-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "21aea948669c92628b49",
+    "url": "/robot-friends/static/css/main.c13022ac.chunk.css"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/robot-friends/static/js/2.b52811a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-friends/static/js/2.b52811a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21aea948669c92628b49",
+    "url": "/robot-friends/static/js/main.ee8af581.chunk.js"
+  },
+  {
+    "revision": "189ba3e7a02064730e17",
+    "url": "/robot-friends/static/js/runtime-main.0c4cd25a.js"
+  }
+]);
